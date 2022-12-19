17605a9b0a68898a761c9e80046209fe3c7bb32e
Beomsun <beomsun22@gmail.com>
2022-12-19 20:53:17 +0900

    feature : user service

--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import (
 	"github/beomsun1234/go-restapi/database"
 	"github/beomsun1234/go-restapi/models"
 	"github/beomsun1234/go-restapi/repository"
+	"github/beomsun1234/go-restapi/service"
 	"log"
+	"strconv"
 
 	fiber "github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(
+		fiber.Config{StrictRouting: true},
+	)
 
 	db := database.NewPostgres()
 
@@ -20,13 +24,13 @@ func main() {
 	db.PostgresDB.AutoMigrate(&models.User{})
 
 	userRepo := repository.NewUserRepository(db.PostgresDB)
-
+	userService := service.NewUserService(userRepo)
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Status(200).SendString("hello")
 	})
 
 	app.Get("/users", func(c *fiber.Ctx) error {
-		users, err := userRepo.FindAllUsers()
+		users, err := userService.FindUsers()
 		if err != nil {
 			return c.Status(404).SendString(err.Error())
 		}
@@ -34,5 +38,32 @@ func main() {
 			"data": users,
 		})
 	})
+	app.Get("/users/search", func(c *fiber.Ctx) error {
+		parma := c.Query("name")
+		user, err := userService.FindUserByName(parma)
+		if err != nil {
+			return c.Status(404).SendString(err.Error())
+		}
+		return c.Status(200).JSON(fiber.Map{
+			"data": user,
+		})
+	})
+	app.Get("/users/:userId", func(c *fiber.Ctx) error {
+		parma := c.Params("userId")
+		id, err := strconv.Atoi(parma)
+
+		if err != nil {
+			return c.Status(404).SendString(err.Error())
+		}
+
+		user, err := userService.FindUserById(id)
+		if err != nil {
+			return c.Status(404).SendString(err.Error())
+		}
+		return c.Status(200).JSON(fiber.Map{
+			"data": user,
+		})
+	})
+
 	log.Fatal(app.Listen(":3000"))
 }
--- repository/user.go
+++ repository/user.go
@@ -7,24 +7,24 @@ import (
 	"gorm.io/gorm"
 )
 
-type UserRepository interface {
+type UserRepositoryInterface interface {
 	FindUserById(user_id int) (*models.User, error)
 	FindUserByName(user_name string) (*models.User, error)
-	FindAllUsers() (*[]models.User, error)
+	FindAllUsers() ([]*models.User, error)
 	CreateUser(user *models.User) (*models.User, error)
 }
 
-type userRepository struct {
+type UserRepository struct {
 	db *gorm.DB
 }
 
-func NewUserRepository(di_db *gorm.DB) UserRepository {
-	return &userRepository{
+func NewUserRepository(di_db *gorm.DB) UserRepositoryInterface {
+	return &UserRepository{
 		db: di_db,
 	}
 }
 
-func (userRepo *userRepository) FindUserById(user_id int) (*models.User, error) {
+func (userRepo *UserRepository) FindUserById(user_id int) (*models.User, error) {
 	find_user := &models.User{}
 	result := userRepo.db.Find(find_user, "id=?", user_id)
 
@@ -35,7 +35,7 @@ func (userRepo *userRepository) FindUserById(user_id int) (*models.User, error) 
 	return find_user, nil
 }
 
-func (userRepo *userRepository) FindUserByName(user_name string) (*models.User, error) {
+func (userRepo *UserRepository) FindUserByName(user_name string) (*models.User, error) {
 	find_user := &models.User{}
 	result := userRepo.db.Find(find_user, "name=?", user_name)
 	if result.RowsAffected < 1 {
@@ -45,17 +45,17 @@ func (userRepo *userRepository) FindUserByName(user_name string) (*models.User, 
 	return find_user, nil
 }
 
-func (userRepo *userRepository) FindAllUsers() (*[]models.User, error) {
-	var find_users []models.User
-	result := userRepo.db.Find(&find_users)
+func (userRepo *UserRepository) FindAllUsers() ([]*models.User, error) {
+	find_users := &[]*models.User{}
+	result := userRepo.db.Find(find_users)
 
 	if result.RowsAffected < 1 {
 		return nil, errors.New("not found user")
 	}
 
-	return &find_users, nil
+	return *find_users, nil
 }
-func (userRepo *userRepository) CreateUser(user *models.User) (*models.User, error) {
+func (userRepo *UserRepository) CreateUser(user *models.User) (*models.User, error) {
 	result := userRepo.db.Create(user)
 	if result.Error != nil {
 		return nil, result.Error
