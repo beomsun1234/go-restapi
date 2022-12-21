e6ecde8a046dab5cf834ce11494a56f9604ea387
Beomsun <beomsun22@gmail.com>
2022-12-21 21:16:37 +0900

    feature : test db 생성(database 캡슐화 해야함)

---
+++ database/sqlite.go
@@ -0,0 +1,28 @@
+package database
+
+import (
+	//"gorm.io/driver/sqlite"
+	"github.com/glebarez/sqlite" // Pure go SQLite driver, checkout https://github.com/glebarez/sqlite for details
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Sqlite struct {
+	SqliteDB *gorm.DB
+}
+
+// github.com/mattn/go-sqlite3
+
+func NewSqlite() *Sqlite {
+	return &Sqlite{}
+}
+
+func (c *Sqlite) Connection() {
+	var err error
+	c.SqliteDB, err = gorm.Open(sqlite.Open("/github.com/beomsun1234/go-restapi/database/sqlitedb/gorm.db"), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		panic(err)
+	}
+}
